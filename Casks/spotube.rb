3717746299ac557efd222623bd6e9148f11c9ab8
Kingkor Roy Tirtho <krtirtho@gmail.com>
2024-06-03 15:13:14 +0600

    feat: Spotube 3.7.0

--- Casks/spotube.rb
+++ Casks/spotube.rb
@@ -1,6 +1,6 @@
 cask "spotube" do
-  version "3.6.0"
-  sha256 "42cafeeba4e8c8b0825308ab8ffed5eafcd2366c7b059dcedf89622f1e807652"
+  version "3.7.0"
+  sha256 "ca79a28181d3216d7d0e3c4a349898e9d9fa7866f21faf59e00c46fbd676bce1"
 
   url "https://github.com/KRTirtho/spotube/releases/download/v#{version}/Spotube-macos-universal.dmg",
       verified: "github.com/KRTirtho/spotube/"
